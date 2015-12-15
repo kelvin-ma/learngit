47d7f6b1e276121a30e1438842facb2430124789
HeJingzhe <hejz0522@163.com>
2015-12-15 15:03:01 +0800

    VO

---
+++ GoldMobile/src/com/csj/gold/mobile/vo/MobileGoldProductVO.java
@@ -0,0 +1,108 @@
+package com.csj.gold.mobile.vo;
+
+public class MobileGoldProductVO {
+	//产品ID
+	private Long productId;
+	//产品名称
+	private String prodcutName;
+	//
+	private double traMinNum;
+	private	double traMaxNum;
+	private double traAddNum;
+	private String pIncomePaymentUnit;
+	private double pInterestRateDay;
+	private double pInterestRateMonth;
+	private double pInterestRateYear;
+	private int pInterestModel;
+	private int pExpiryDay;
+	private int pExpiryMonth;
+	private int pExpiryYear;
+	private String imageFilePath;
+	public Long getProductId() {
+		return productId;
+	}
+	public void setProductId(Long productId) {
+		this.productId = productId;
+	}
+	public String getProdcutName() {
+		return prodcutName;
+	}
+	public void setProdcutName(String prodcutName) {
+		this.prodcutName = prodcutName;
+	}
+	public double getTraMinNum() {
+		return traMinNum;
+	}
+	public void setTraMinNum(double traMinNum) {
+		this.traMinNum = traMinNum;
+	}
+	public double getTraMaxNum() {
+		return traMaxNum;
+	}
+	public void setTraMaxNum(double traMaxNum) {
+		this.traMaxNum = traMaxNum;
+	}
+	public double getTraAddNum() {
+		return traAddNum;
+	}
+	public void setTraAddNum(double traAddNum) {
+		this.traAddNum = traAddNum;
+	}
+	public String getpIncomePaymentUnit() {
+		return pIncomePaymentUnit;
+	}
+	public void setpIncomePaymentUnit(String pIncomePaymentUnit) {
+		this.pIncomePaymentUnit = pIncomePaymentUnit;
+	}
+	public double getpInterestRateDay() {
+		return pInterestRateDay;
+	}
+	public void setpInterestRateDay(double pInterestRateDay) {
+		this.pInterestRateDay = pInterestRateDay;
+	}
+	public double getpInterestRateMonth() {
+		return pInterestRateMonth;
+	}
+	public void setpInterestRateMonth(double pInterestRateMonth) {
+		this.pInterestRateMonth = pInterestRateMonth;
+	}
+	public double getpInterestRateYear() {
+		return pInterestRateYear;
+	}
+	public void setpInterestRateYear(double pInterestRateYear) {
+		this.pInterestRateYear = pInterestRateYear;
+	}
+	public int getpInterestModel() {
+		return pInterestModel;
+	}
+	public void setpInterestModel(int pInterestModel) {
+		this.pInterestModel = pInterestModel;
+	}
+	public int getpExpiryDay() {
+		return pExpiryDay;
+	}
+	public void setpExpiryDay(int pExpiryDay) {
+		this.pExpiryDay = pExpiryDay;
+	}
+	public int getpExpiryMonth() {
+		return pExpiryMonth;
+	}
+	public void setpExpiryMonth(int pExpiryMonth) {
+		this.pExpiryMonth = pExpiryMonth;
+	}
+	public int getpExpiryYear() {
+		return pExpiryYear;
+	}
+	public void setpExpiryYear(int pExpiryYear) {
+		this.pExpiryYear = pExpiryYear;
+	}
+	public String getImageFilePath() {
+		return imageFilePath;
+	}
+	public void setImageFilePath(String imageFilePath) {
+		this.imageFilePath = imageFilePath;
+	}
+	
+	
+	
+}
