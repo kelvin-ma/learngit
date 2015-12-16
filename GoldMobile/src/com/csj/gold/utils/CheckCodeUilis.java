109bb4f8de69c42f0493eca4fd49339df8743f76
kelvin-ma <kelvin.ma@foxmail.com>
2015-12-16 14:56:14 +0800

    验证码工具类

---
+++ GoldMobile/src/com/csj/gold/utils/CheckCodeUilis.java
@@ -0,0 +1,43 @@
+package com.csj.gold.utils;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+/**
+ * 验证码工机类
+ * @author kelvin
+ * @version 1.0。0
+ */
+public class CheckCodeUilis {
+	/**
+	 * 默认大小
+	 */
+	private static final Integer SIZE=4;
+	
+	private static final Integer MAX_SIZE=19;
+	
+	private static final Integer MIX_SIZE=1;
+	/**
+	 * 获取数字随机验证码
+	 * 
+	 * @param length 验证码长度为空默认大小是4 最大长度19 最小长度1
+	 * @return
+	 */
+	public static String numberChenKCode(Integer length){
+		
+		if(length==null||length<MIX_SIZE){
+			length=SIZE;
+		}
+		if(length>MAX_SIZE){
+			new Exception("The length of the verification code can not exceed the maximum value");
+		}
+		
+		Random random =new Random();
+		Long randomNumber= random.nextLong();
+		String randomNumberStr =randomNumber.toString();
+		String numStr= randomNumberStr.substring(randomNumberStr.length()-length, randomNumberStr.length());
+		return numStr;
+	}
+
+}
