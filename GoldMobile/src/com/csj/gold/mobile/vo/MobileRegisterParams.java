4f85770faf2bf8777c048731b8f23395bf6c9aa9
kelvin <kelvin.ma@foxmail.com>
2015-12-29 15:03:11 +0800

    注册用VO

---
+++ GoldMobile/src/com/csj/gold/mobile/vo/MobileRegisterParams.java
@@ -0,0 +1,35 @@
+package com.csj.gold.mobile.vo;
+
+public class MobileRegisterParams extends MobileParams {
+	private String phone;
+	
+	private String checkCode;
+	
+	private String password;
+	
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getCheckCode() {
+		return checkCode;
+	}
+
+	public void setCheckCode(String checkCode) {
+		this.checkCode = checkCode;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	
+}
